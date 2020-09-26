e9ef018ce0b9426f8c14af903ed29fe66db3444e
Futa MIYACHI <miyachf@gmail.com>
2020-09-26 17:05:52 +0900

    線形探索に戻した

--- src/JisyoFile.scala
+++ src/JisyoFile.scala
@@ -4,21 +4,19 @@ import java.io.File
 import scala.io.{Codec, Source}
 import scala.util.chaining.scalaUtilChainingOps
 import scala.util.control.Exception.nonFatalCatch
-import scala.annotation.tailrec
 
 final case class JisyoFile(file: File, okuriAriRange: Option[Range], okuriNasiRange: Option[Range], isDesc: Boolean) {
-  import JisyoFile.{binSearchEntry, entryLinePtrn, isOkuriAri}
+  import JisyoFile.{entryLinePtrn, isOkuriAri}
 
   def convert(midashi: String)(implicit codec: Codec): Vector[String] =
-    (
-      for {
-        searchRange <- if (isOkuriAri(midashi)) okuriAriRange else okuriNasiRange
-        lines = (Source fromFile file).getLines().slice(searchRange.start, searchRange.end).toVector
-        result <- binSearchEntry(midashi, lines, isDesc)
-      } yield result
-    ) map {
-      case entryLinePtrn(_, cands) => (cands split '/').toVector
-    } getOrElse Vector.empty
+    ( for {
+      searchRange <- if (isOkuriAri(midashi)) okuriAriRange else okuriNasiRange
+      //entry <- binSearchEntry(midashi, this.file, searchRange, isDesc)
+      searchStr = s"$midashi "
+      entry <- (Source fromFile this.file).getLines().slice(searchRange.start, searchRange.end) find (_ startsWith searchStr)
+      entryLinePtrn(_, cands) = entry
+    } yield (cands split '/').toVector
+    ) getOrElse Vector.empty
 }
 
 object JisyoFile {
@@ -42,7 +40,7 @@ object JisyoFile {
         if (lines.length == okuriAriSignIndex + 1) None
         else Some((okuriNasiSignIndex + 1) until lines.length)
 
-      Right(JisyoFile(file, okuriAriRange, okuriNasiRange, isDesc = false /* TODO: desc/asc */ ))
+      Right(JisyoFile(file, okuriAriRange, okuriNasiRange, isDesc = true /* TODO: desc/asc */ ))
     }
   }
 
@@ -50,39 +48,4 @@ object JisyoFile {
 
   def isOkuriAri(midashi: String): Boolean =
     ('a' to 'z') contains midashi.last
-
-  @inline private def extractMidashi(entry: String): String =
-    entry takeWhile (_ != ' ')
-
-  @inline private def compareMidashies(m1: String, m2: String): Int = {
-    def _impl(m1: List[Char], m2: List[Char]): Int =
-      (m1, m2) match {
-        case (Nil, Nil) => 0
-        case (Nil, _)   => -1
-        case (_, Nil)   => 1
-        case (h1 :: t1, h2 :: t2) =>
-          (h1 compareTo h2) match {
-            case 0 => _impl(t1, t2)
-            case c => c
-          }
-      }
-    _impl(m1.toList, m2.toList)
-  }
-
-  def binSearchEntry(midashi: String, lines: Vector[String], isDesc: Boolean): Option[String] = {
-    val ordCoef = if (isDesc) 1 else -1
-    @tailrec def _impl(range: Range): Option[String] =
-      range.size match {
-        case 0 => None
-        case len => {
-          val idx = len / 2 + range.start
-          (compareMidashies(midashi, extractMidashi(lines(idx))) * ordCoef) match {
-            case 0          => Some(lines(idx))
-            case c if c < 0 => _impl(range.start until idx)
-            case c if c > 0 => _impl(idx + 1 until range.end)
-          }
-        }
-      }
-    _impl(lines.indices)
-  }
 }
