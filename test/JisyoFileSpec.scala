e08fdb1bc48897af8c03c5843824b9a8f995338c
Futa MIYACHI <miyachf@gmail.com>
2020-10-06 14:34:40 +0900

    jisyofile spec

---
+++ test/JisyoFileSpec.scala
@@ -0,0 +1,19 @@
+package skkserv
+
+import org.scalatest.wordspec.AnyWordSpec
+
+class JisyoFileSpec extends AnyWordSpec {
+
+  "#convert" should {
+    "convert properly" in {
+      val jf = new JisyoFile(
+        okuriAriEntries = Vector("あi /合/会/"),
+        okuriNasiEntries = Vector("あい /愛/相/")
+      )
+
+      assert(jf.convert("あi") == Some(Vector("合", "会")))
+      assert(jf.convert("あい") == Some(Vector("愛", "相")))
+      assert(jf.convert("ない") == None)
+    }
+  }
+}
